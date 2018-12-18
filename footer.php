25379bb69ebbcc60d748fc8410968fa271cd802f
yuma_united <y-iwakata@supercrowds.co>
2018-12-18 15:24:44 +0900

    footer/header調整

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
+
+    <?php wp_footer(); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/javascripts/application.js"></script>
   </body>
-  <?php wp_footer(); ?>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-  <script src="<?php bloginfo('template_directory'); ?>/assets/javascripts/application.js"></script>
 </html>
--- header.php
+++ header.php
@@ -3,16 +3,14 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-  <meta name="robots" content="index,follow,noarchive">
-  <meta name="googlebot" content="index,follow,noarchive">
 
-  <link rel="apple-touch-icon-precomposed" href="<?php bloginfo('url'); ?>/assets/images/favicon/webclip.png">
-  <meta name="msapplication-TileImage" content="<?php bloginfo('url'); ?>/assets/images/favicon/webclip.png">
-  <link rel="icon" href="<?php bloginfo('url'); ?>/assets/images/favicon/favicon.ico" sizes="32x32">
+  <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/webclip.png">
+  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/webclip.png">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon/favicon.ico" sizes="32x32">
   <meta name="msapplication-TileColor" content="#ef9fc2">
   <meta name="theme-color" content="#ef9fc2">
 
-  <link rel="stylesheet" href="<?php bloginfo('url'); ?>/assets/stylesheets/application.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/stylesheets/application.css">
 
   <?php wp_head(); ?>
 
@@ -32,8 +30,8 @@
         },
         "headline": "<?php wp_title(); ?> ",
         "name": "<?php wp_title(); ?> ",
-        "description": "<?php the_field('subtitle'); ?>",
-        "alternateName": "<?php the_field('subtitle'); ?>",
+        "description": "<?php the_content(); ?>",
+        "alternateName": "<?php the_content(); ?>",
         "ArticleSection": "Fashion",
         "image": {
             "@type": "ImageObject",
@@ -61,50 +59,6 @@
     </script>
   <?php endif; ?>
 
+</head>
+
 <body>
-
-  <!--
-    Category Link
-    <?php echo get_category_link( '1' ); ?>
-    <?php echo get_category_link( $category_id ); ?>
-
-    Thumbnails
-    <?php the_post_thumbnail('thumbnail'); ?>
-
-    ACF
-    <?php the_field('subtitle'); ?>
-
-    Author Name
-    <?php the_author_posts_link(); ?>
-
-    Author Image form ACF
-    <?php $author_id = get_the_author_meta( 'ID' ); ?>
-    URL : <?php echo get_author_posts_url( $author_id ); ?>
-    Image : <?php the_field('author-image', 'user_'. $author_id ); ?>
-
-    Get Category List
-    <?php echo get_the_category_list( ' ,' ); ?>
-
-    Social Buttons
-    <div class="l-share-area">
-      <?php
-        $url_encode=urlencode(get_permalink());
-        $title=strip_tags(get_the_title());
-        $subtitle=strip_tags(get_field('subtitle'));
-        $twitter_text=mb_substr($subtitle.' '.$title,0,87);
-      ?>
-      <h3 class="small-headline">Share of This page!!</h3>
-      <div class="l-sns-share-area">
-        <a href="http://www.facebook.com/share.php?u=<?php echo $url_encode; ?>" onclick="window.open(this.href, 'FBwindow', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;" target="_blank" class="p-share-button facebook-share">
-          Share
-        </a>
-        <a href="http://twitter.com/share?url=<?php echo $url_encode; ?>&amp;text=<?php echo $twitter_text; ?>" target="_blank" class="p-share-button twitter-share">
-          Tweet
-        </a>
-      </div>
-    </div>
-
-    Related Article by Yet Another
-    <?php related_posts(); ?>
-
-  -->
